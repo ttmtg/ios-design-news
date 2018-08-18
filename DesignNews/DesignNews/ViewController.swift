3205fb9d54d3b8b6e3d3384a0bc05de3fe0d6108
高橋知嗣 <tomotsugu00kaiin@gmail.com>
2018-08-18 18:55:44 +0900

    新規プロジェクトを作成

---
+++ DesignNews/DesignNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DesignNews
+//
+//  Created by 高橋知嗣 on 2018/08/18.
+//  Copyright © 2018年 ttmtg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
